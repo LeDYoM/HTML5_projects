30f9f101151626be74c851908cc1d14ad0298e35
Ismael <ismael.gonzalez@scoreloop.com>
2014-05-19 16:38:31 +0200

    WIP

--- personal/public_html/sfme/sfmecore.js
+++ personal/public_html/sfme/sfmecore.js
@@ -1,17 +1,84 @@
 (function()
 {
-
-    var THIS = this;
-
-    function defined(x)
+    this.byId =  function(id_)
     {
-        return typeof x !== 'undefined';
-    }
+        return document.getElementById(id_);
+    };
+    this.byClass = function(class_)
+    {
+        return document.getElementsByClassName(class_);
+    };
+    this.byTag = function(class_)
+    {
+        // Warning: this method returns an array.
+        return document.getElementsByTagName(class_);
+    };
+    this.on = function(id_,event_,callback_)
+    {
+        var elem = null;
+        if (typeof id_ === 'object')
+        {
+            elem = id_;
+        }
+        else
+        {
+            elem = this.byId(id_);
+            if (!elem)
+            {
+                elem = this.byClass(id_);
+                if (!elem)
+                {
+                    elem = this.byTag(id_);
+                    if (elem)
+                    {
+                        elem = elem[0];
+                    }
+                }
+            }
+        }
 
+        if (elem)
+        {
+            elem.addEventListener(event_,callback_);
+        }
+        return elem;
+    };
+    this.isObject = function(obj)
+    {
+        return (typeof obj === "obj");
+    };
+    this.typeName = function(obj)
+    {
+        return Object.prototype.toString.call(obj).slice(8, -1);        
+    };
+    this.isType = function(obj,typeName)
+    {
+        return this.typeName(obj) === typeName;
+    };
+}
+).apply(cns("sfme.utils"));
+
+(function()
+{
+    var utils = cns("sfme.utils");
     this.ready = false;
+    this.canvas = null;
 
-    this.init = function()
+    this.init = function(options)
     {
+        options = options || { };
+        
+        if (utils.isType(options.canvas, "canvas"))
+        {
+            // There is a valid canvas object. We can use it.
+            this.canvas = options.canvas;
+        } else
+        {
+            // There is no canvas object. Let's create it.
+            var parent = (utils.isType(options.parent, "div")) ? options.parent : utils.byTagName("body");
+            
+            
+        }
         this.ready = true;
     };
 
@@ -19,20 +86,15 @@
     {
         alert("works!");
     };   
-
-    var options = {
-        x : 123,
-        y : 'abc'
-        };
-
-    this.define = function(key, value)
+    
+    function createCanvas(parent,id_,w_,h_)
     {
-        if(defined(options[key]))
-        {
-            options[key] = value;
-        }
-    };
-    
-    
-
-}).apply(cns("sfme.core"));
+        var temp = document.createElement("canvas");
+        temp.id = id_;
+        temp.width = w_;
+        temp.height = h_;
+        parent.appendChild(temp);
+        return temp;
+    }    
+}
+).apply(cns("sfme.core"));

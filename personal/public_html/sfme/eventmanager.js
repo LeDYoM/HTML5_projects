027c78cbbdd1dc0fa4235f7dc0494558575c190c
unknown <ledyom@hotmail.com>
2014-06-06 08:41:59 +0200

    Preparing eventmanager.

---
+++ personal/public_html/sfme/eventmanager.js
@@ -0,0 +1,31 @@
+(function()
+{
+    var ready = false;
+    this.init = function()
+    {
+        ready = true;
+    };
+    
+    function subscribe(eventName,eventHandler)
+    {
+        document.addEventListener("sfme."+eventName, eventHandler, false);
+    }
+    this.subscribe = subscribe;
+    
+    function launchEvent(eventName,eventObject)
+    {
+        var event = new CustomEvent("sfme."+eventName, {
+            detail: {
+                    message: "",
+                    time: new Date(),
+                    data: eventObject
+            },
+            bubbles: true,
+            cancelable: true
+        });
+
+        document.dispatchEvent(event);
+    }
+    this.launchEvent = launchEvent;
+}
+).apply(cns("sfme.internal.eventManager"));

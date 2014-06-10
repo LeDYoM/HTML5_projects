cb83aec4477e0c79b5a316363e62447ef97d46b6
unknown <ledyom@hotmail.com>
2014-06-10 11:19:58 +0200

    Examples fixed.

--- personal/public_html/example2/example2.js
+++ personal/public_html/example2/example2.js
@@ -32,39 +32,40 @@ cns("sfme.userModules").defineUserModule("example2", "main",
                 angle: 45.0,
                 ratio: "normal",
                 zNear: 0.1,
-                zFar: 100.0
+                zFar: 100.0,
+                objects3d: [
+                    {
+                        id: "triangle",
+                        shapeType: "triangle_normal",
+                        width: 2.0,
+                        height: 2.0,
+                        position: [-1.5, 0.0, -7.0],
+                        material: {
+                            name: "color_pass",
+                            colors: [
+                                1.0, 1.0, 0.0, 1.0,
+                                0.0, 1.0, 0.0, 1.0,
+                                0.0, 0.0, 1.0, 1.0
+                            ]
+                        }
+                    },
+                    {
+                        id: "quad",
+                        shapeType: "quad_normal",
+                        width: 2.0,
+                        height: 2.0,
+                        position: [0.5, 0.0, -5.0],
+                        material: {
+                            name: "textured",
+                            textureMode: "attach",
+                            color: [1.0, 0.5, 1.0, 1.0],
+                            texture: "test"
+                        }
+                    }
+                ]
             },
             backgroundColor: [1.0, 1.0, 0.0, 1.0],
-            objects: [
-                {
-                    id: "triangle",
-                    shapeType: "triangle_normal",
-                    width: 2.0,
-                    height: 2.0,
-                    position: [-1.5, 0.0, -7.0],
-                    material: {
-                        name: "color_pass",
-                        colors: [
-                            1.0, 1.0, 0.0, 1.0,
-                            0.0, 1.0, 0.0, 1.0,
-                            0.0, 0.0, 1.0, 1.0
-                        ]
-                    }
-                },
-                {
-                    id: "quad",
-                    shapeType: "quad_normal",
-                    width: 2.0,
-                    height: 2.0,
-                    position: [0.5, 0.0, -5.0],
-                    material: {
-                        name: "textured",
-                        textureMode: "attach",
-                        color: [1.0, 0.5, 1.0, 1.0],
-                        texture: "test"
-                    }
-                }
-        ]});
+        });
 
     };
 }

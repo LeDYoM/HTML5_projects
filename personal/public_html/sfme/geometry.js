5436860ba08ea5cf750a2dd0f08ff3a11b8dc654
unknown <ledyom@hotmail.com>
2014-07-08 11:09:31 +0200

    A bit of clean up.

--- personal/public_html/sfme/geometry.js
+++ personal/public_html/sfme/geometry.js
@@ -61,25 +61,7 @@
         Quad: 1,
         CubeType0: 2
     };
-    
-    function transformVertex(matrix,v)
-    {
-        return mat4.multiplyVec3(matrix,v);
-    }
-    
-    function transformVertexArray(matrix,vArray)
-    {
-        var result = [];
-        
-        for (var i=0;i<vArray.length;++i)
-        {
-            var vat = [vArray[i],vArray[i+1],vArray[i+2]];
-            result = result.concat(transformVertex(matrix,vat));
-            i+=2;
-        }
-        return result;
-    }
-    
+       
     function rotationMatrix(params)
     {
         var matrix = mat4.identity(mat4.create());
@@ -101,8 +83,6 @@
                                                     [-1.0,1.0,1.0]],size);
                 mesh.addFace(obj,qVertex);
                 
-                var matrix = mat4.identity(mat4.create());
-                obj.vertex = transformVertexArray(matrix,qVertex);
                 break;
             case this.MeshType.Triangle:
                 var qVertex = _.addVertexFaceFromCenter([[0.0,1.0,1.0],
@@ -110,8 +90,6 @@
                                                 [1.0,-1.0,1.0]],size);
                 mesh.addFace(obj,qVertex);
 
-                var matrix = mat4.identity(mat4.create());
-                obj.vertex = transformVertexArray(matrix,qVertex);
                 break;
             case this.MeshType.CubeType0:
                 var qVertex = _.addVertexFaceFromCenter([[-1.0,-1.0,1.0], [1.0,-1.0,1.0], [1.0,1.0,1.0], [-1.0,1.0,1.0]],size);
@@ -148,40 +126,6 @@
                         axis: [0,-1,0]
                     },                    
                 ],qVertex);
-/*
-                obj.vertex = processVertex(rotationMatrix,[
-                    // Front face
-                    {
-                        angle: 0,
-                        axis: [0,0,0]
-                    },
-                    // Back face
-                    {
-                        angle: Math.PI,
-                        axis: [0,1,0]
-                    },
-                    // Top face
-                    {
-                        angle: Math.PI/2,
-                        axis: [-1,0,0]
-                    },
-                    // Bottom face
-                    {
-                        angle: Math.PI/2,
-                        axis: [1,0,0]
-                    },
-                    // Right face
-                    {
-                        angle: Math.PI/2,
-                        axis: [0,1,0]
-                    },
-                    // Left face
-                    {
-                        angle: Math.PI/2,
-                        axis: [0,-1,0]
-                    },                    
-                ],qVertex);
-                */
                 break;
         }
         obj.vertex = mesh.getMeshVertexArray(obj);
